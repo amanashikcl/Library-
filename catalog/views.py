0d90b7550342bf8f61a303b4a8c1f5b48eb1970f
amanashikcl <amanashikcloud@outlook.com>
2026-01-15 12:53:02 +0530

    added pagination

--- catalog/views.py
+++ catalog/views.py
@@ -11,8 +11,12 @@ def index(request):
     num_instances = Book.objects.all().count()
     num_instances_available = (BookInstance.objects.filter(status__exact='a').count())
     num_authors = Author.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     context = {'num_books': num_books, 'num_instances': num_instances,
-               'num_instances_available': num_instances_available, 'num_authors': num_authors
+               'num_instances_available': num_instances_available, 'num_authors': num_authors,
+               'num_visits': num_visits,
                }
 
     return render(request, 'index.html', context=context)
@@ -22,9 +26,11 @@ class BookListView(generic.ListView):
     queryset = Book.objects.filter(title__icontains='node')
     template_name = 'book_list.html'
     model = Book
+    paginate_by = 5
 
 
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'book_detail.html'
+
 
